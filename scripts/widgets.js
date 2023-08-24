17b34c8250e6c8b4bbbb1dbc0bc5fed72c3e2264
Mateusz Rak <109479736+MateuszRak0@users.noreply.github.com>
2023-08-24 13:41:37 +0200

    Add Widgets & messages Module

---
+++ scripts/widgets.js
@@ -0,0 +1,195 @@
+// Base
+const widgetsMenager = {
+    toHide:false,
+
+    refreshPositions:function(){
+        for(let widget of this.activeWidgets){
+            widget.zIndex = 50 + this.activeWidgets.indexOf(widget);
+        }
+    },
+
+    hideImportant:function(){
+        if(this.toHide) this.toHide.hide();
+    },
+
+    loadFromDocument:function(){
+        for(let element of document.getElementsByClassName("floating-widget")){ 
+            if(!this.allWidgets.has(element.id))
+             new FloatingWidget(element);
+        }
+    }
+}
+
+widgetsMenager.allWidgets = new Map();
+widgetsMenager.activeWidgets = new Set();
+
+for(let btn of document.getElementsByName("show-widget-btn")){
+    btn.addEventListener("click",(e)=>{
+        let widget = widgetsMenager.allWidgets.get(e.target.value);
+        if(widget) widget.switchVisibility();
+    })
+}
+
+class Widget{
+    constructor(element){
+        if(widgetsMenager.allWidgets.has(element.id)) return widgetsMenager.allWidgets.get(element.id);
+        this.element = element;
+        this.hidden = true;
+        this.id = this.element.id;
+        widgetsMenager.allWidgets.set(this.id,this);
+    }
+
+    set zIndex(value){
+        this.element.style.zIndex = value;
+    }
+
+    show(saveToMemory,hideImportant){
+        this.hidden = false;
+        this.checkPosition();
+        if(!this.element.classList.contains("active")){
+            this.element.classList.add("active");
+            if(hideImportant) widgetsMenager.hideImportant();
+            if(saveToMemory) widgetsMenager.toHide = this;
+            widgetsMenager.activeWidgets.add(this);
+        } 
+    }
+
+    checkPosition(){
+        let offScreen = false;
+        if(this.element.offsetLeft + this.element.offsetWidth/2 > appWindow.offsetWidth || this.element.offsetLeft < 0) offScreen = true;
+        if(this.element.offsetTop + this.element.offsetHeight/2 > appWindow.offsetHeight || this.element.offsetTop < 0) offScreen = true;
+
+        if(offScreen){
+            this.element.style.left = `${60}px`;
+            this.element.style.top = `${60}px`;
+        }
+    }
+
+    hide(){
+        this.hidden = true;
+        if(this.element.classList.contains("active")){
+            this.element.classList.remove("active");
+            this.hidden = true;
+            widgetsMenager.activeWidgets.delete(this);
+        } 
+    }
+
+    switchVisibility(){
+        (this.element.classList.contains("active") ) ? this.hide() : this.show() ;
+    }
+}
+
+class FloatingWidget extends Widget{
+    constructor(element){
+        super(element)
+        let cancelBtn = document.createElement("button");
+        cancelBtn.type = "button";
+        cancelBtn.classList.add("icon-cancel");
+        cancelBtn.classList.add("btn-cancel");
+        cancelBtn.classList.add("additional-btn");
+        cancelBtn.value = this.id;
+
+        this.element.appendChild(cancelBtn);
+        cancelBtn.addEventListener("click",this.hide.bind(this))
+
+        this.minimalizeBtn = document.createElement("button");
+        this.minimalizeBtn.type = "button";
+        this.minimalizeBtn.classList.add("icon-resize-small");
+        this.minimalizeBtn.classList.add("btn-minimalize");
+        this.minimalizeBtn.classList.add("additional-btn");
+        this.minimalizeBtn.value = this.id;
+
+        this.element.appendChild(this.minimalizeBtn);
+        this.minimalizeBtn.addEventListener("click",this.minimalize.bind(this))
+    }
+
+    minimalize(event){
+        let btn = event.target;
+
+        if(btn.classList.contains("icon-resize-small")){
+            btn.classList.remove("icon-resize-small");
+            btn.classList.add("icon-resize-full");
+        }
+
+        else{
+            btn.classList.add("icon-resize-small");
+            btn.classList.remove("icon-resize-full");
+        }
+
+        this.element.classList.contains("minimalize") ? this.element.classList.remove("minimalize") : this.element.classList.add("minimalize");
+    }
+
+}
+
+// Message Widget Functions
+const messenger = new class{
+    constructor(){
+        this.acceptFunc=false;
+        this.rejectFunc=false;
+        this.showed=false;
+        this.allMessages={};
+    
+        this.elements={
+            title:document.getElementById("msg-title"),
+            content:document.getElementById("msg-content"),
+            rejectBtn:document.getElementById("msg-reject"),
+            acceptBtn:document.getElementById("msg-accept"),
+        };
+
+        this.elements.acceptBtn.addEventListener("click",this.accept.bind(this));
+        this.elements.rejectBtn.addEventListener("click",this.reject.bind(this));
+    }
+
+
+    show(msgID,acceptFunc,rejectFunc){
+        let message = this.allMessages[msgID];
+        if(message){
+            this.updateContent(message);
+        }
+        else{
+            this.updateContent(this.allMessages.messageNotFound);
+        }
+        this.showed = true;
+        this.acceptFunc = acceptFunc;
+        this.rejectFunc = rejectFunc;
+        this.widget.show()
+    }
+
+    updateContent(message){
+        this.elements.title.innerHTML = message.title;
+        this.elements.content.innerHTML = message.content;
+        this.elements.rejectBtn.innerHTML = message.rejectBtn;
+        this.elements.acceptBtn.innerHTML = message.acceptBtn;
+    }
+
+    accept(){
+        if( typeof this.acceptFunc === "function") this.acceptFunc();
+        this.hide();
+    }
+    
+    reject(){
+        if( typeof this.rejectFunc === "function" ) this.rejectFunc();
+        this.hide();
+    }
+
+    hide(){
+        this.widget.hide();
+        this.showed = false;
+    }
+}
+
+messenger.widget = new Widget( document.getElementById("msg-box") );
+
+messenger.allMessages.messageNotFound = {
+    title: "Dziwna Sprawa",
+    content: "Zgubiliśmy gdzieś tę wiadomość ! Ale pewnie wykonujesz jakąs operacje czy chcesz ją zakceptować ? ",
+    acceptBtn: "Tak",
+    rejectBtn:"Nie"
+}
+
+messenger.allMessages.loadingError = {
+    title: "Błąd Ładowania",
+    content: "Podczas pobierania danych z serwera wystąpił jakiś problem, Konieczne jest załadowania strony ponownie. Jeżeli błąd nadal występuje skontaktuj się ze mną",
+    acceptBtn: "Załaduj ponownie",
+    rejectBtn: "Wyjdź",
+}
